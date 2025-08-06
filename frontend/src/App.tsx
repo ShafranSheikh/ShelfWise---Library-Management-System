f561ee804beda040bb9a4941da0c950b14f9cfe2
Mohamed Shafran <shafransheikh@gmail.com>
2025-08-06 07:38:45 +0530

    mount Navbar component in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,15 @@ import '../src/App.css'
 import HomePage from './pages/HomePage';
 import AddBookPage from './pages/AddBookPage';
 import BookDetailsPage from './pages/BookDetailsPage';
+import NavBar from './components/NavBar';
 function App() {
   return (
     <Router>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path='/addbook' element={<AddBookPage />} />
-        <Route path='/bookdetails::bookId' element={<BookDetailsPage />} />
+        <Route path='/bookdetails:bookId' element={<BookDetailsPage />} />
       </Routes>
     </Router>
   )
