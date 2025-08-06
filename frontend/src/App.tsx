19b3fe1b5910c4249dfed864cf41c6b01d9f8257
Mohamed Shafran <shafransheikh@gmail.com>
2025-08-06 06:50:34 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '../src/App.css'
+import HomePage from './pages/HomePage';
+import AddBookPage from './pages/AddBookPage';
+import BookDetailsPage from './pages/BookDetailsPage';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path='/addbook' element={<AddBookPage />} />
+        <Route path='/bookdetails::bookId' element={<BookDetailsPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
