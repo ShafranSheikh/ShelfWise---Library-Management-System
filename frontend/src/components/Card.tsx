ab9cf94d0470f3d332da0561ece9d057697ac939
Mohamed Shafran <shafransheikh@gmail.com>
2025-08-06 20:38:05 +0530

    add navigation to book details page

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,9 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 interface CardProps {
     imageurl?: string;
     title?: string;
     author?: string;
 }
 const Card = ({imageurl, title, author}:CardProps) => {
+    const navigate = useNavigate();
   return (
     <div className='max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-5'>
         <a href="#">
@@ -14,7 +16,7 @@ const Card = ({imageurl, title, author}:CardProps) => {
                 <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>{title}</h5>
             </a>
             <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{author}</p>
-            <button className=' w-full inline-flex items-center justify-center px-3 py-2 bg-[#C8B1E4] text-sm font-medium text-center text-[#2F184B]  rounded-lg  hover:bg-[#2F184B] hover:text-[#C8B1E4] transition-colors duration-300'>
+            <button onClick={()=>navigate('/bookdetails')} className=' w-full inline-flex items-center justify-center px-3 py-2 bg-[#C8B1E4] text-sm font-medium text-center text-[#2F184B]  rounded-lg  hover:bg-[#2F184B] hover:text-[#C8B1E4] transition-colors duration-300'>
                 View
             </button>
         </div>    
