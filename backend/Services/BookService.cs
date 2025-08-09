9d3f5b2496b8e2ef47c995df5a7a72ee4ad9e945
Mohamed Shafran <shafransheikh@gmail.com>
2025-08-09 20:36:40 +0530

    add business logic related to book

--- backend/Services/BookService.cs
+++ backend/Services/BookService.cs
@@ -1,6 +1,53 @@
-﻿namespace backend.Services
+﻿using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+namespace backend.Services
 {
     public class BookService
     {
+        private readonly AppDbContext _context;
+        public BookService(AppDbContext context)
+        {
+            _context = context;
+        }
+        //Add New Book Record
+        public async Task<Book> AddAsync(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return book;
+        }
+
+        //Get All Books
+        public async Task<List<Book>> GetAllAsync()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        //Get Book By Id
+        public async Task<Book?> GetByIdAsync(int id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+
+        //Update Book Record
+        public async Task UpdateAsync(Book book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        // Delete Book Data
+        public async Task DeleteAsync(int id)
+        {
+            var bookData = await _context.Books.FindAsync(id);
+            if (bookData != null) 
+            {
+                _context.Books.Remove(bookData);
+                await _context.SaveChangesAsync();  
+            }
+        }
+
     }
 }
