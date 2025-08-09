d092b285f9ed62c3cba84fda8d093690ef498e52
Mohamed Shafran <shafransheikh@gmail.com>
2025-08-09 20:35:29 +0530

    add model for book data

--- backend/Models/Book.cs
+++ backend/Models/Book.cs
@@ -1,6 +1,19 @@
-﻿namespace backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace backend.Models
 {
     public class Book
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Author { get; set; } = string.Empty;
+        [Required]
+        public string AboutAuthor { get; set; } = string.Empty;
+        [Required]
+        public string BookDescription { get; set; } = string.Empty;
+        [Required]
+        public byte[] Image { get; set; } = Array.Empty<byte>();  
     }
 }
