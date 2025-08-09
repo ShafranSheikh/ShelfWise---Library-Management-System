578982210c3e94c19f03462d97709e5f760911cc
Mohamed Shafran <shafransheikh@gmail.com>
2025-08-09 21:05:28 +0530

    add logic for add new book to the collection

--- frontend/src/pages/AddBookPage.tsx
+++ frontend/src/pages/AddBookPage.tsx
@@ -1,7 +1,56 @@
+import { useState } from 'react'
 import listImg from '../assets/List_Add.png'
 import PrimaryButton from '../components/PrimaryButton';
 import coverImage from '../assets/CoverImage.png'
+import axios from 'axios';
 const AddBookPage = () => {
+  const [formData, setFormData] = useState<{
+      title: string;
+      author: string;
+      aboutAuthor: string;
+      image: File | null;
+      bookDescription: string;
+  }>({
+      title: '',
+      author: '',
+      aboutAuthor: '',
+      image: null,
+      bookDescription: ''
+  });
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    if (name === 'image' && 'files' in event.target) {
+      const files = (event.target as HTMLInputElement).files;
+      setFormData({ ...formData, [name]: files && files[0] ? files[0] : null });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  }
+  const handleFormSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const formDataToSend = new FormData();
+    formDataToSend.append('title', formData.title);
+    formDataToSend.append('author', formData.author);
+    formDataToSend.append('aboutAuthor', formData.aboutAuthor);
+    if (formData.image) {
+      formDataToSend.append('image', formData.image);
+    }
+    formDataToSend.append('bookdescription', formData.bookDescription);
+    try{
+      await axios.post('http://localhost:5008/api/book', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+      });
+      alert('Book Added successfully');
+      window.location.reload();
+    }catch (error){
+      console.error('Error adding book:', error);
+      alert('Failed to add book. Please try again.');
+    }
+}
   return (
     <div className='min-h-screen flex items-center justify-center bg-background my-8'>
       <div className='bg-card p-8 rounded-xl shadow-lg flex flex-col md:flex-row w-full max-w-5xl bg-[#F4EFFA]'>
@@ -12,37 +61,57 @@ const AddBookPage = () => {
           <img src={listImg} alt="" />
         </div>
 
-        <form action="" className='font-body'>
+        <form action="" className='font-body' onSubmit={handleFormSubmit}>
 
           <label htmlFor="">Book Title</label>
           <input type="text"
             className='w-full p-2 mb-5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#C8B1E4]' 
             placeholder='Enter book title' 
+            required
+            onChange={handleChange}
+            name='title'
+            value={formData.title}
           />
 
           <label htmlFor="">Author</label>
           <input type="text" 
             className='w-full p-2 mb-5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#C8B1E4]' 
-            placeholder='Enter author name' 
+            placeholder='Enter author name'
+            required
+            onChange={handleChange}
+            name='author'
+            value={formData.author}
           />
 
           <label htmlFor="">About author</label>
           <textarea 
             className='w-full p-2 mb-5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#C8B1E4] resize-none' 
             placeholder='Write about the author' 
-            rows={5} 
+            rows={5}
+            required
+            onChange={handleChange}
+            name='aboutAuthor'
+            value={formData.aboutAuthor}
           />
 
           <label htmlFor="">Book Cover Image</label>
           <input type="file"
-            className='w-full p-2 mb-5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#C8B1E4]' 
+            className='w-full p-2 mb-5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#C8B1E4]'
+            onChange={handleChange}
+            name='image'
+            accept="image/png, image/jpeg"
+            required 
           />
 
           <label htmlFor="">Book Description</label>
           <textarea 
             className='w-full p-2 mb-5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#C8B1E4] resize-none' 
             placeholder='Write a brief description of the book' 
-            rows={4} 
+            rows={4}
+            required
+            onChange={handleChange}
+            name='bookDescription'
+            value={formData.bookDescription}
           />
           <div className="flex justify-center mb-8">
               <PrimaryButton label='Add Book' type='submit' className='border-2 bg-[#2F184B] text-[#F4EFFA] hover:bg-[#F4EFFA] hover:text-[#2F184B]' />
@@ -50,7 +119,7 @@ const AddBookPage = () => {
         </form>
         </div>  
         <div className="hidden md:flex items-center justify-center w-1/2">
-          <img src={coverImage} alt="Doctor" className="max-w-full" />
+          <img src={coverImage} alt="cover" className="max-w-full" />
         </div>
       </div>
     </div>
