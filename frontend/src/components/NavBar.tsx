73cea629346d1d75db6e0b740b98921d710e8d0c
Mohamed Shafran <shafransheikh@gmail.com>
2025-08-09 21:10:11 +0530

    add link to login and sign out

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,8 +1,18 @@
-
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import logo from '../assets/logo.png'
 import logoName from '../assets/ShelfWise.png'
 const NavBar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // State to manage login status
+  useEffect(() =>{
+    const token = localStorage.getItem('token')
+    setIsLoggedIn(!!token); // Check if token exists to determine login status
+  },[])
+  const handleLogout = ( ) => {
+    localStorage.removeItem('token'); // Remove token from local storage
+    setIsLoggedIn(false); // Update login status
+    window.location.href = '/login'; // Redirect to homepage
+  }
   const navigate = useNavigate();
   return (
     <nav className='bg-[#532B88] shadow-md px-6 py-4 flex justify-between items-center'>
@@ -13,7 +23,16 @@ const NavBar = () => {
       <ul className='flex space-x-9 font-bold '>
         <li  className='font-display text-[#F4EFFA]'><a href="/">Home</a></li>
         <li className='font-display text-[#F4EFFA]'><a href="/addbook">Add Book</a></li>
-        <li  className='font-display text-[#F4EFFA]'><a href="/login">Login</a></li>
+        {isLoggedIn ? (
+          <>
+            <button className='font-display text-[#F4EFFA]' onClick={handleLogout}>Sign out</button>
+          </>
+        ):(
+          <>
+            <li  className='font-display text-[#F4EFFA]'><a href="/login">Login</a></li>
+          </>
+        )}
+        
       </ul>
     </nav>
   )
