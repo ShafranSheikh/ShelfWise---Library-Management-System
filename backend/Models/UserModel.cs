05c986dcdfdee7c7201dd11b10b41b887391fb1d
Mohamed Shafran <shafransheikh@gmail.com>
2025-08-09 20:54:10 +0530

    add model for the users

---
+++ backend/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string firstName { get; set; } = string.Empty;
+        [Required]
+        public string lastName { get; set; } = string.Empty;
+        [Required]
+        public string email { get; set; } = string.Empty;
+        [Required]
+        public string password { get; set; } = string.Empty;
+        [Required]
+        public int age { get; set; } 
+    }
+}
