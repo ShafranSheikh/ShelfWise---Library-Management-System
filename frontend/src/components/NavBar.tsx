0edee6af109d9f35067998718ced7151099f033d
Mohamed Shafran <shafransheikh@gmail.com>
2025-08-10 07:40:31 +0530

    improved navbar mobile responsiveness

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -15,12 +15,12 @@ const NavBar = () => {
   }
   const navigate = useNavigate();
   return (
-    <nav className='bg-[#532B88] shadow-md px-6 py-4 flex justify-between items-center'>
+    <nav className='bg-[#532B88] shadow-md px-6 py-4 flex flex-col  sm:flex-row justify-between items-center'>
       <div className='flex flex-row items-center space-x-4'>
         <img onClick={()=> navigate('/')} src={logo} alt="" className='h-10' />
         <img onClick={()=> navigate('/')} src={logoName} />
       </div>
-      <ul className='flex space-x-9 font-bold '>
+      <ul className='flex mt-5 sm:mt-0  space-x-9 font-bold '>
         <li  className='font-display text-[#F4EFFA]'><a href="/">Home</a></li>
         <li className='font-display text-[#F4EFFA]'><a href="/addbook">Add Book</a></li>
         {isLoggedIn ? (
